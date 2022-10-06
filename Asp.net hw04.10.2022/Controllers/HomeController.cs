64179065d52e47d7d8420b459514e41dcd0b75fa
Tamerlan9129 <tamerlankha@code.edu.az>
2022-10-06 11:39:49 +0400

    Add project files.

---
+++ Asp.net hw04.10.2022/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.net_hw04._10._2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Tamerlan";
+            ViewBag.Surname = "Alimardanli";
+            ViewBag.Age = 31;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "+994557623800";
+            return View();
+        }
+    }
+}
---
+++ Asp.net hw04.10.2022/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.DataProtection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.MapControllerRoute(
+
+    name: "default",
+    pattern: "{controller=home}/{action=index}"
+    
+    );
+
+
+
+app.Run();
